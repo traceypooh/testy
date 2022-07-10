f70c356da9ce2baa3a6b9978836f8d75dbd9102f
Tracey Jaquith <tracey@archive.org>
2022-07-10 18:28:38 -0400

    basic parse of sitemap

--- _site/theme.js
+++ _site/theme.js
@@ -1,2 +1,14 @@
-const lines = document.getElementsByTagName('body')[0].innerHTML.split('\n')
-console.log({ lines })
+// eslint-disable-next-line no-console
+const log = console.log.bind(console)
+
+
+async function main() {
+  const lines = document.getElementsByTagName('body')[0].innerHTML.split('\n')
+  log({ lines })
+
+  const urls = (await (await (await fetch('../../../sitemap.xml')).text())).split('<loc>').slice(1).map((e) => e.split('</loc>').slice(0, 1).join(''))
+  log({ urls })
+}
+
+// eslint-disable-next-line no-void
+void main()
